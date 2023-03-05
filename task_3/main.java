65dc06b6c7f92e4d1fff2c9400d9e805a6c62cb3
Alexey <Lexuse2007@gmail.com>
2023-03-05 13:00:23 +0300

    task_3 finished

---
+++ task_3/main.java
@@ -0,0 +1,45 @@
+package task_3;
+import java.util.Scanner;
+
+/**
+ * Реализовать простой калькулятор
+ */
+
+public class main {
+    public static void main(String[] args) {
+        Scanner iSсanner = new Scanner(System.in);
+        System.out.println("Добро пожаловать в программу калькулятор");
+        System.out.println("Вам доступны простые математические операции + - * / ");
+
+        System.out.printf("Введите первое число");
+        double a = iSсanner.nextLong();
+
+        System.out.printf("Введите символ оператора +, -, *, или /: " );
+        char operator = iSсanner.next().charAt(0);
+
+        System.out.printf("Введите второе число : ");
+        double b = iSсanner.nextLong();
+
+        System.out.println("результат операции : " + result(a, b, operator));
+
+    }
+
+    public static double result (double a, double b, char oper) {
+        double res = 0;
+        switch (oper) {
+            case '+':
+                res = a + b;
+                break;
+            case '-':
+                res = a - b;
+                break;
+            case '*':
+                res = a * b;
+                break;
+            case '/':
+                res = a / b;
+                break;
+        }
+        return res;
+    }
+}
