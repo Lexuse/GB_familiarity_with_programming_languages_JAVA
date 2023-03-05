ef5f37234136e49ce4b3f67d4c7c851e6ce6e5c0
Alexey <Lexuse2007@gmail.com>
2023-03-05 09:15:18 +0300

    task_1, task_2 finished

---
+++ task_1/main.java
@@ -0,0 +1,36 @@
+package task_1;
+import java.util.Scanner;
+
+/**
+  Вычислить [n-ое треугольного число](сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+ */
+public class main {
+
+    public static void main(String[] args) {
+        Scanner iscanner = new Scanner(System.in);
+
+        int from = 1;
+        System.out.printf("Введите число: ");
+        int to = iscanner.nextInt();
+
+        System.out.println("Сумма чисел от 1го до " + to + " равна: " + sumInRange(from, to));
+        System.out.println("Произведение чисел от 1го до " + to + " равна: " + factInRange(from, to));
+    }
+    public static long sumInRange(long from, long to) {
+        long number = 0;
+        for (long i = from; i<to; i++){
+             number += i;
+        }
+        return number;
+    }
+
+    public static long factInRange(long from, long to) {
+        long number = 1;
+        for (long i = from; i<to; i++){
+            number *= i;
+        }
+        return number;
+    }
+
+
+}
---
+++ task_2/main.java
@@ -0,0 +1,21 @@
+package task_2;
+
+/**
+ Вывести все простые числа от 1 до 1000
+ */
+
+
+public class main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 1000; ++i) {
+            int count = 0;
+            for (int j = 2; j <= i && count < 2; ++j) {
+                if (i % j == 0) {
+                    ++count;
+                }
+            }
+            if (count < 2)
+                System.out.println(i);
+        }
+    }
+}
