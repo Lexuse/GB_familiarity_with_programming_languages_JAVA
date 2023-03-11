7b5db5501418c1f4af303ff2a132e095d7b2522b
Alexey <Lexuse2007@gmail.com>
2023-03-11 16:14:46 +0300

    Seminar 2 task_3 finished

---
+++ Seminar_2_task_3/main.java
@@ -0,0 +1,37 @@
+package Seminar_2_task_3;
+
+/*
+    Напишите метод, который принимает на вход строку (String) и определяет является ли строка
+    палиндромом (возвращает boolean значение)
+ */
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите строку для проверки ее на палиндром. Как пример слово \"шалаш\" : ");
+        String userText = iScanner.next();
+        userText = userText.replace(" ", ""); //убираем все пробелы из строки
+
+        if (ChekPalindrom(userText)){
+            System.out.println("Строка является палиндромом");
+        }
+        else
+            System.out.println("Строка не палиндром!");
+
+    }
+
+        public static boolean ChekPalindrom(String string){
+            char[] userTextChar = string.toCharArray();
+            for (int i = 0; i < string.length()/2; i++) {
+                if (userTextChar[i] == userTextChar[userTextChar.length - i -1])
+                    continue;
+                else
+                    return false;
+            }
+            return true;
+        }
+
+    }
+
