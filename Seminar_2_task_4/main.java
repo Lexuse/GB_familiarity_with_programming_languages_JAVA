484b7b123ffd71e90c06475136fc4f6cfcc06685
Alexey <Lexuse2007@gmail.com>
2023-03-11 17:36:37 +0300

    Seminar 2 task_4 finished

---
+++ Seminar_2_task_4/main.java
@@ -0,0 +1,29 @@
+package Seminar_2_task_4;
+
+/*
+    Напишите метод, который составит строку, состоящую из 100 повторений слова TEST и метод,
+    который запишет эту строку в простой текстовый файл, обработайте исключения.
+ */
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) {
+        String text = "TEST";
+        StringBuilder string = new StringBuilder();
+        string.append(text.repeat(100));
+
+
+        try (FileWriter fw = new FileWriter("file.txt", false)) {
+            for (int i = 0; i < 100; i++) {
+                fw.write(text + '\n');
+            }
+            fw.flush();
+        }
+        catch (IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+}
