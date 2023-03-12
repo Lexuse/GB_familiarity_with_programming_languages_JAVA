e41aeb90799ffbf7d7b751201f02f072ef4832de
Alexey <Lexuse2007@gmail.com>
2023-03-12 18:28:00 +0300

    package Seminar_3_task_1 finished

--- Seminar_3_task_1/main.java
+++ Seminar_3_task_1/main.java
@@ -1,9 +1,6 @@
 package Seminar_3_task_1;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /*
     Заполнить список десятью случайными числами. Отсортировать список методом sort() и вывести его на экран.
@@ -13,7 +10,9 @@ public class main {
         List<Integer> numList = new ArrayList<>(10);
         FillList(numList);
         System.out.println(numList);
-        System.out.println(numList.stream().sorted().toList());
+        //System.out.println(numList.stream().sorted().toList());
+        Collections.sort(numList);
+        System.out.println(numList);
 
     }
 
