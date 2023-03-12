a020dab45e14537480f2d973c9148738a0de558c
Alexey <Lexuse2007@gmail.com>
2023-03-12 14:39:30 +0300

    Seminar_2_HomeWork_task_4 finished

--- task_3/main.java
+++ task_3/main.java
@@ -1,12 +1,23 @@
 package task_3;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Реализовать простой калькулятор
  */
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Logger logger = Logger.getLogger(Seminar_2_HomeWork_task_2.main.class.getName());
+        FileHandler fh = new FileHandler("log.txt");
+        logger.addHandler(fh);
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+
         Scanner iSсanner = new Scanner(System.in);
         System.out.println("Добро пожаловать в программу калькулятор");
         System.out.println("Вам доступны простые математические операции + - * / ");
@@ -20,27 +31,32 @@ public class main {
         System.out.printf("Введите второе число: ");
         double b = iSсanner.nextLong();
 
-        System.out.println("результат операции : " + result(a, b, operator));
+        System.out.println("результат операции : " + result(a, b, operator, logger));
 
     }
 
-    public static double result (double a, double b, char oper) {
+    public static double result (double a, double b, char oper, Logger logger) {
         double res = 0;
         switch (oper) {
             case '+':
                 res = a + b;
+                logger.info("Выполнена операция сложения");
                 break;
             case '-':
                 res = a - b;
+                logger.info("Выполнена операция вычитания");
                 break;
             case '*':
                 res = a * b;
+                logger.info("Выполнена операция умножения");
                 break;
             case '/':
                 res = a / b;
+                logger.info("Выполнена операция днлнния");
                 break;
             default:
                 System.out.printf("Вы ошиблись при вводе оператора!  " );
+                logger.info("Пользователь ошибся при вводе оператора");
         }
         return res;
     }
