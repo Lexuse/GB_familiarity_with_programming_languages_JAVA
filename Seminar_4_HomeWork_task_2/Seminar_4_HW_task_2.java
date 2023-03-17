829a11849741a6443e4cccdb29947dc66a98cd93
Lexuse <Lexuse2007@gmail.com>
2023-03-17 10:46:53 +0300

    fix errors

--- Seminar_4_HomeWork_task_2/Seminar_4_HW_task_2.java
+++ Seminar_4_HomeWork_task_2/Seminar_4_HW_task_2.java
@@ -2,6 +2,7 @@ package Seminar_4_HomeWork_task_2;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 /*
     Реализуйте очередь с помощью Linkedlist со следующими методами:
@@ -10,6 +11,7 @@ import java.util.Queue;
  */
 public class Seminar_4_HW_task_2 {
     public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
         Queue<Integer> list = new LinkedList<>();
         list.add(1);
         list.add(2);
@@ -17,28 +19,33 @@ public class Seminar_4_HW_task_2 {
         list.add(4);
         list.add(5);
         System.out.println("Исходная очередь: " + list);
+        System.out.print("Введите целое число, которое нужно поместить в конец очереди  ");
+        int userInt = iScanner.nextInt();
 
-        Enqueue(list);
-        Dequeue(list);
-        First(list);
+        Enqueue(list, userInt);
+        System.out.println("Результат работы метода enqueue() помещает элемент" + userInt + " в конец очереди: " + list);
+        int dequeue = Dequeue(list);
+        System.out.println("Результат работы метода dequeue() возвращает первый элемент из очереди и удаляет его: " + dequeue + " " + list);
+        int first = First(list);
+        System.out.println("Результат работы метода first() возвращает первый элемент из очереди, не удаляя его: " + first + "  " + list);
     }
 
-    private static void First(Queue<Integer> list) {
+    private static  int First(Queue<Integer> list) {
         //возвращает первый элемент из очереди, не удаляя
-        System.out.println("Результат работы метода first() возвращает первый элемент из очереди, не удаляя его: " + list.peek() + "  " + list);
+        return list.peek();
 
     }
 
-    private static void Dequeue(Queue<Integer> list) {
+    private static int Dequeue(Queue<Integer> list) {
         //возвращает первый элемент из очереди и удаляет его
-        //list.peek();
+        int first =list.peek();
         list.remove();
-        System.out.println("Результат работы метода dequeue() возвращает первый элемент из очереди и удаляет его: " + list);
+        return first;
     }
 
-    private static void Enqueue(Queue<Integer> list) {
+    private static void Enqueue(Queue<Integer> list, int integer) {
         //помещает элемент в конец очереди
-        list.add(6);
-        System.out.println("Результат работы метода enqueue() помещает элемент \"6\" в конец очереди: " + list);
+        list.add(integer);
+
     }
 }
