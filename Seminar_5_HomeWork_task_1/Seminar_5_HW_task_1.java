63c425ac53f174c6ec8f07a8aa6d653d8128f735
Alexey <Lexuse2007@gmail.com>
2023-03-18 15:21:07 +0300

    Seminar_5_HomeWork_task_1 finished

--- Seminar_5_HomeWork_task_1/Seminar_5_HW_task_1.java
+++ Seminar_5_HomeWork_task_1/Seminar_5_HW_task_1.java
@@ -1,11 +1,8 @@
 package Seminar_5_HomeWork_task_1;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.*;
 
+import java.util.*;
 import static java.lang.System.in;
-import static java.lang.System.setOut;
 
 /*
     Реализуйте структуру телефонной книги с помощью HashMap, учитывая, что 1 человек может иметь несколько телефонов.
@@ -13,84 +10,73 @@ import static java.lang.System.setOut;
 public class Seminar_5_HW_task_1 {
     public static void main(String[] args) {
         int helloNumb = 0;
-        menu(helloNumb);
+        Map<String, String> map = new HashMap<>();
+        menu(helloNumb, map);
     }
-
-
-
-    public static void menu (int helloNumb) {
+    public static void menu (int helloNumb, Map<String, String> map) {
 
         if (helloNumb == 0){
             System.out.println("Добро пожаловать в телефонный справочник");
-            helloNumb = helloNumb + 1;
         }
-        String menu = "Введите команду для работы с телефонной книгой\n" +
-                "1. - начать заполнять книгу\n" +
-                "2. - просмотр книги\n" +
-                "3. - завершить работу программы";
+        String menu = """
+                Введите команду для работы с телефонной книгой
+                1. - начать заполнять книгу
+                2. - просмотр книги
+                3. - завершить работу программы""";
 
         System.out.println(menu);
-        Map<Integer, String> bookMap = new HashMap<>();
         Scanner iScanner = new Scanner(in);
-        //String userData = "";
         boolean isQuit = false;
         while (!isQuit) {
             byte function = iScanner.nextByte();
             switch (function) {
-                case 1:
-                    AddData(bookMap); //userData);
-                    break;
-                case 2:
-                    PrintBook(bookMap);
-                    // System.out.println(bookMap);
-                    break;
-                case 3:
-                    isQuit = true;
-                    break;
-                default:
+                case 1 -> AddData(map); //userData);
+                case 2 -> PrintBook(map);
+                case 3 ->isQuit = true;
+                default -> {
                     System.out.println("вы ввели несуществующий пункт меню");
-                    menu(1);
+                    restart(map);
+                }
             }
         }
+
+        //restart(bookMap);;
     }
-    public static void restart () {
-        menu(1);
+    public static void restart (Map<String, String> map) {
+        menu(1, map);
     }
 
-    public static Map AddData (Map map) {        //String userData){
+    public static void AddData (Map<String, String> map) {        //String userData){
         Scanner iScanner = new Scanner(in);
-        List<String> wordsMass = new ArrayList<>();
-        List<Integer> phoneNumbers = new ArrayList<>();
         System.out.println("Вы находитесь в режиме добавления данных в книгу. Для выхода в меню напишите exit \nВведите Имя абонента и его телефоны через запятую:");
         String input = iScanner.nextLine();
-        input = iScanner.next();
-        int numberTemp = 0;
-        String fioTemp = "";
+        String phoneToString = "";
         String[] wordsToSplit = input.split(",");
-        for (int i = 0; i < wordsToSplit.length; i++) {
-                try {
-                    int num = Integer.parseInt(wordsToSplit[i]);
-                    phoneNumbers.add(num);
-                } catch (NumberFormatException e) {
-                    //System.out.println("NumberFormatException: " + e.getMessage());
-                    wordsMass.add(wordsToSplit[i]);
-                }
-                map.put(fioTemp, numberTemp);
-            }
-            restart();
-        return map;
+        String fio = wordsToSplit[0];
+
+        for (int i = 1; i < wordsToSplit.length; i++) {
+            phoneToString += " " + (wordsToSplit[i] + ";");
+        }
+        //iScanner.close();
+        map.put(fio, phoneToString);
+        restart(map);
     }
+        private static void PrintBook (Map<String, String> map){
+            System.out.println("Телефонная книга содержт " + map.size() + " записей:");
+            if (!map.isEmpty()) {
+                for (Object entry: map.entrySet()) {
+                    System.out.println(entry);
+                }
+                System.out.println("\n");
 
-
-        private static void PrintBook (Map bookMap){
-            if (!(bookMap.isEmpty())) {
-                System.out.println(bookMap);
             }
             else {
                 System.out.println("Книга абосолютна пуста. Сначала добавьте данные");
             }
-            restart();
+            restart(map);
         }
+
+
     }
 
 
